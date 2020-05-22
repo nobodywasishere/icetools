a5256c224e592eceb08324d671c3fc371012ccca
nobodywasishere <m.a.riegert@snet.net>
2020-05-22 19:16:48 -0400

    add cmake

--- icetools.sh
+++ icetools.sh
@@ -8,11 +8,11 @@ UNAME_STR=`uname`
 
 echo "Installing dependencies..."
 if [[ "$UNAME_STR" == "Darwin" ]] && hash brew 2>/dev/null; then
-	brew update && brew install python3 libftdi0 libffi autoconf bison gawk gnu-sed graphviz xdot mercurial
+	brew update && brew install python3 libftdi0 libffi autoconf bison gawk gnu-sed graphviz xdot mercurial cmake
 fi
 if [[ "$UNAME_STR" == "Linux" ]] && hash apt-get 2>/dev/null; then
 	sudo apt-get install -y pkg-config build-essential bison flex gawk tcl-dev libffi-dev git mercurial python python3 libftdi-dev
-    sudo apt-get install -y libreadline-dev clang graphviz xdot
+    sudo apt-get install -y libreadline-dev clang graphviz xdot cmake
 fi
 
 echo "┌──────────┐"
